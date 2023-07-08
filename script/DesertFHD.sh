a08843dc80b5726433a25973204fbb64ed487c7c
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-07-08 22:35:47 +0300

    Delete DesertFHD.sh
    r

--- script/DesertFHD.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-##########################################################
-version= 2.0
-description="DesertFHD V2.0\n 1- Fix some missing screens\n 2- Fix missing CAM icons\n 3- Fix missing weather icons\n 4- Fix some Renders and Converters\n 5- Fix slow menu issue for some device\n 6- Fix auto Update feature in Plugin"
-#########################################################
-# ----------------------------------------------------------------------------------------------------------
