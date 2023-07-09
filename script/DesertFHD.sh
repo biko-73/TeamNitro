ce60064221fb4ee52e31377641ce977ac6c5dcd8
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-07-09 23:36:09 +0300

    R

--- script/DesertFHD.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-##########################################################
-version= 3.0
-description="DesertFHD V3.0\n 1- pro_skin without poster (Basic)\n 2- pro_skin PosterX (TNPosterX)\n 3- pro_skin by xtraEvent\n 4- NEW screens for OpenATV 7.4, OpenBH, Egami.\n 5- Fix some bug\n 6- Improve Performance of skin\n"
-#########################################################
-# ----------------------------------------------------------------------------------------------------------
