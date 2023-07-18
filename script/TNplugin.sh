211549d023c0405f59f38be9f47e24b551c75a33
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-07-18 21:52:07 +0300

    Update TNplugin.sh
    v_3.1

--- script/TNplugin.sh
+++ script/TNplugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##########################################################
-version=3.0
+version=3.1
 description="TeamNitro Control Center\n 1-Fix some bug\n 2-Upgrade Plugin for DesertFHD Additional Screens\n 3-New Team Spinner added by Oktus\n" !!!
 #########################################################
 # ----------------------------------------------------------------------------------------------------------
