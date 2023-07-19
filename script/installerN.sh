b28b466ee381dcb701d00b8c5fbbf8986890abf3
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-07-19 22:02:53 +0300

    FIX_4

---
+++ script/installerN.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#########################################################
+version=5.4
+description="Nitro !!!"
+#########################################################
+
+#########################################################
+PACKAGE_DIR='TeamNitro/main/skins'
+MY_FILE="NitroAdvanceFHD.tar.gz"
+#########################################################
+
+##############################################################################################################
+##
+## Script Purpose: Download and install TeamNitro skin
+##
+## Command: wget https://raw.githubusercontent.com/biko-73/TeamNitro/main/script/installerN.sh -O - | /bin/sh
+##
+##############################################################################################################
+
+MY_MAIN_URL="https://raw.githubusercontent.com/biko-73/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+if [ -f $MY_TMP_FILE ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xf $MY_TMP_FILE -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "#########################################################"
+        echo "#  NitroAdvanceFHD Skin $version INSTALLED SUCCESSFULLY #"
+        echo "#              BY NitroTeam - support on                #"
+        echo "#  https://www.tunisia-sat.com/forums/threads/4334347/  #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
+fi
+# ------------------------------------------------------------------------------------------------------------
