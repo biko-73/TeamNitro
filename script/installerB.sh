2cbdf3ed70efefaf55f331ec258906ebdaca78db
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-12-30 23:55:37 +0200

    Update installerB.sh

--- script/installerB.sh
+++ script/installerB.sh
@@ -20,7 +20,7 @@ echo $MY_SEP
 echo ''
 
 wget -O /var/volatile/tmp/TeamNitro.tar.gz --no-check-certificate "https://github.com/biko-73/TeamNitro/raw/main/skins/TeamNitro.tar.gz"
-wget -O /var/volatile/tmp/Team_Nitro-by_BoHlala-V_1.0.tar.gz --no-check-certificate "https://github.com/biko-73/TeamNitro/raw/main/skins/Team_Nitro-by_BoHlala-V_1.0.tar.gz.tar.gz"
+wget -O /var/volatile/tmp/Team_Nitro-by_BoHlala-V_1.0.tar.gz --no-check-certificate "https://github.com/biko-73/TeamNitro/raw/main/skins/Team_Nitro-by_BoHlala-V_1.0.tar.gz"
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/TeamNitro"
 rm -rf "/usr/share/enigma2/BoHLALA_FHD"
 
