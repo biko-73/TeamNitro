a00a655c6abf1c081da082faf4eeb79084b71c27
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-12-31 02:48:12 +0200

    Update installerB.sh

--- script/installerB.sh
+++ script/installerB.sh
@@ -6,10 +6,9 @@ description="*** TEAMNITRO by BoHlala V1.0 ***\n Fix some issues related to xml 
 #########################################################
 
 #########################################################
-PACKAGE_DIR='TeamNitro/main'
+PACKAGE_DIR='TeamNitro/main/skins/'
 MY_FILE="Team_Nitro-by_BoHlala-V_1.0.tar.gz" "TeamNitro.tar.gz"
 #########################################################
-
 MY_MAIN_URL="https://raw.githubusercontent.com/biko-73/"
 MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
 MY_TMP_FILE="/tmp/"$MY_FILE
